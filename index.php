a2df39ddb13f64dd627affca676846899797bb52
Joshualeexy <oghenekevwejoshualee@gmail.com>
2023-12-16 13:58:47 +0100

    Fixed  overflow , added media query

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+if (isset($_POST['submitbtn']) && $_SERVER['REQUEST_METHOD'] == 'POST') {
+  session_start();
+  if (empty($_POST['name'])) {
+    echo "<script> alert('Name field cannot be empty')</script>";
+  } else {
+    $name = htmlspecialchars($_POST['name']);
+    $_SESSION['name'] = $name;
+    header("location:options.php");
+  }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Home &nbsp; &nbsp;| &nbsp; Easy Solve</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<main id="">
+
+  <body>
+
+    <header>
+      <nav class="nav-bars">
+        <div class="logo">EASY Solve</div>
+      </nav>
+    </header>
+    <section>
+      <div class="">
+        <h1 id="hh">WELCOME</h1>
+      </div>
+      <form action="" method="POST">
+        <div class="">
+          <input type="text" name="name" id="name" placeholder="Enter your name" class="namefield">
+        </div>
+        <div class="">
+          <button name="submitbtn" class="submitbtn">Submit</button>
+        </div>
+
+      </form>
+
+
+    </section>
+    <script src="script.js"></script>
+
+</main>
+</body>
+
+</html>
---
+++ options.php
@@ -0,0 +1,42 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>EazySolve Options</title>
+  <link rel="stylesheet" href="calculate.css" />
+
+</head>
+
+<body>
+  <section>
+    <div class="header2" id="header">
+      welcome <span id="namepage2" style="color:blue"><?= $_SESSION['name'] ?></span> what are you solving for today.
+    </div>
+
+    <ul class="mathlist">
+      <li>
+        <a href="temperature-converter.html" target="_blank">temperatue</a>
+      </li>
+      <li>
+        <a href="tax.html" target="_blank">Tax</a>
+      </li>
+      <li>
+        <a href="workdone.html" target="_blank">Work-done</a>
+      </li>
+      <li>
+        <a href="sqrt.html" target="_blank">Square root</a>
+      </li>
+      <li>
+        <a href="displacement.html" target="_blank">Displacement</a>
+      </li>
+      <li>
+        <a href="velocity.html" target="_blank">Average Velocity</a>
+      </li>
+    </ul>
+  </section>
+</body>
+
+</html>
